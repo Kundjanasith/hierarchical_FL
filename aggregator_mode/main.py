b05d6d18b9820396096bbc5759f9d14964991bd2
Kundjanasith Thonglek <thonglek.kundjanasith.ti7@is.naist.jp>
2022-09-09 20:30:33 +0900

    ip

--- aggregator_mode/main.py
+++ aggregator_mode/main.py
@@ -2,7 +2,9 @@ import sys, os, time, glob, copy
 sys.path.insert(1, '..')       
 import utils
 
-local_ip = ['10.10.100.33', '10.10.100.34']
+local_ip = ['10.10.100.34', '10.10.100.35']
+global_ip = '10.10.100.33'
+
 init_model = utils.model_init()
 init_model.save_weights('global_models/aggregated_model_ep0.h5')
 while not os.path.exists('global_models/aggregated_model_ep0.h5'):
@@ -13,7 +15,7 @@ NUM_ROUNDS = 10
 NUM_DEVICES = 2
 for e in range(NUM_ROUNDS):
     for n in range(NUM_DEVICES):
-        utils.receive_model('localhost',19192,'local_models/')
+        utils.receive_model(global_ip,19192,'local_models/')
     arr = []
     for p in glob.glob('local_models/*_ep%d.h5'%e):
         model = utils.model_init()
