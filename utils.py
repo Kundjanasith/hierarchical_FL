b19d1c644134d8925329c1c1cbd06aa953862913
Kundjanasith Thonglek <thonglek.kundjanasith.ti7@is.naist.jp>
2022-09-09 21:29:18 +0900

    async send model

--- utils.py
+++ utils.py
@@ -28,6 +28,7 @@ class ClientThread(Thread):
                 if not bytes_read:    
                     break
                 f.write(bytes_read)
+            f.close()
         time.sleep(1)
 
 def send_model(tcp_ip, tcp_port, file_path):
@@ -44,6 +45,7 @@ def send_model(tcp_ip, tcp_port, file_path):
             if not bytes_read:
                 break
             s.sendall(bytes_read)
+        f.close()
     time.sleep(1)
     s.close()
     return 'complete'
